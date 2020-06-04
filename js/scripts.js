81aa5d7744e9cf51f519eb5e70fa99842c50f11a
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-06-04 14:44:21 -0700

    add input fields and UI logic add celebrity
    Co-authored-by: Michael Petersen <michael_petersen_email@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,35 @@
 $(document).ready(function() {
-  $("form#yourInfo").submit(function() {
+  $("form#yourInfo").submit(function(event) {
     const name = $("#inputName").val();
+    const band = $("#inputBand").val();
+    const horoscope = $("#inputHoroscope").val();
+    const food = $("#inputFood").val();
 
     if (name === "") {
-      $("#yourInfoDiv").addClass("has-error");
+      $("#yourNameDiv").addClass("has-error");
       $("span#helpBlock1").text("Please enter your name");
     }
-    else {
 
+    if (band === "") {
+      $("#yourBandDiv").addClass("has-error");
+      $("span#helpBlock2").text("Please enter your favorite band");
     }
+
+    if (horoscope === "1") {
+      $("#yourHoroscopeDiv").addClass("has-error");
+      $("span#helpBlock3").text("Please enter your sign");
+    }
+
+    if (food === "") {
+      $("#yourFoodDiv").addClass("has-error");
+      $("span#helpBlock4").text("Please enter your favorite food");
+    }
+
+    if (name !== "" && band !== "" && horoscope !== "1" && food !== "") {
+      $("form#yourInfo").toggle();
+      $("#everybodyLovesDevito").show();
+    }
+
     event.preventDefault();
   });
 });
